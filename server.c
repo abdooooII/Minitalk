8c162cc5fb4a621dd44e1ca4bd17ad8b8fb102d8
Abderrahmane Ouafsou <abouafso@e2r4p12.1337.ma>
2024-03-18 02:20:34 +0000

    server function

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: abouafso <abouafso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 01:37:34 by abouafso          #+#    #+#             */
-/*   Updated: 2024/03/16 20:42:10 by abouafso         ###   ########.fr       */
+/*   Updated: 2024/03/18 01:53:41 by abouafso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,38 @@
 void server_home(void)
 {
     
-    
+    ft_putchar_fd('\n', 1);
     ft_putstr_fd("\033[0;32m", 1);
     ft_putstr_fd("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n", 1);
-    printf("\033[0;34m");
+    ft_putstr_fd("\033[0;34m", 1);
     ft_putchar_fd('\n', 1);
-    printf("███████ ███████ ██████  ██    ██ ███████ ██████  \n");
-    printf("██      ██      ██   ██ ██    ██ ██      ██   ██ \n");
-    printf("███████ █████   ██████  ██    ██ █████   ██████  \n");
-    printf("     ██ ██      ██   ██  ██  ██  ██      ██   ██ \n");
-    printf("███████ ███████ ██   ██   ████   ███████ ██   ██ \n");
+    ft_putstr_fd("███████ ███████ ██████  ██    ██ ███████ ██████  \n", 1);
+    ft_putstr_fd("██      ██      ██   ██ ██    ██ ██      ██   ██ \n", 1);
+    ft_putstr_fd("███████ █████   ██████  ██    ██ █████   ██████  \n", 1);
+    ft_putstr_fd("     ██ ██      ██   ██  ██  ██  ██      ██   ██ \n", 1);
+    ft_putstr_fd("███████ ███████ ██   ██   ████   ███████ ██   ██ \n", 1);
     ft_putstr_fd("\033[0;31m", 1);
     ft_putstr_fd("                  PID : ", 1);
     ft_putnbr(getpid());
     ft_putchar_fd('\n', 1);
     ft_putstr_fd("\033[0;32m", 1);
     ft_putstr_fd("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n", 1);
+    ft_putchar_fd('\n', 1);
     ft_putstr_fd("\033[0m", 1);
 }
+
 
 int main(int ac, char **av)
 {
     (void)av;
+	
+	struct sigaction	sig;
+	sig.sa_sigaction = 
+	sig.sa_flags = SA_SIGINFO;
+	
     if(ac != 1)
         return(1);
+
+	siginfo_t ;
     server_home();
 }
